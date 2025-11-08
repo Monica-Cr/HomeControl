9afbb4f721680382ca6d37ba31b63d07bd0237f9
Monica-Cr <Monicalex200@gmail.com>
2025-11-08 16:17:40 +0100

    Added HomeAssistantController in Infrastructure

---
+++ HomeControl.Infrastructure/Clients/HomeAssistantClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Diagnostics;
+
+namespace HomeControl.Infrastructure.Clients
+{
+    public class HomeAssistantClient : HttpClient
+    {
+        private void GetDevces()
+        {
+            GetAsync("/api/devices");
+            throw new NotImplementedException();
+        }
+    }
+}
